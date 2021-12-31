12b3b29226d987209c3f35b3de113ecd7d85436d
Moussa Haidous <haidous.m@gmail.com>
2021-12-31 14:06:02 +0000

    fix shell script to get SNAPSHOt image

--- .ci/deploy-traefiker.sh
+++ .ci/deploy-traefiker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 exec > /home/haidousm/hooks/traefiker/output.log 2>&1
-docker pull haidousm/traefiker:1.0.0
+docker pull haidousm/traefiker:SNAPSHOT
 cd /home/haidousm/haidousm.com/traefik
 docker-compose up -d
 docker system prune -f
