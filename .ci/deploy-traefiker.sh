fa6a49ecca543ce65806562f4627bde49baab49c
haidousm <haidous.m@gmail.com>
2022-01-09 16:10:02 +0200

    change file paths

--- .ci/deploy-traefiker.sh
+++ .ci/deploy-traefiker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 exec > /home/haidousm/hooks/traefiker/output.log 2>&1
 docker pull haidousm/traefiker:SNAPSHOT
-cd /home/haidousm/haidousm.com/traefik
+cd /home/haidousm/
 docker-compose up -d
 docker system prune -f
